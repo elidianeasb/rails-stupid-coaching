ddd28d6069af60fa19b22e41bab37a17f1c09e57
Elidiane Bezerra <elidiane.asb@gmail.com>
2023-08-14 14:21:29 +0100

    added style to stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,11 +9,11 @@ class QuestionsController < ApplicationController
 
   def answer
     if ask.downcase == "i am going to work"
-      @answer = "Great!"
+      @answer = "Great! 😃"
     elsif ask.include? "?"
-      @answer = "Silly question, get dressed and go to work!"
+      @answer = "Silly question, get dressed and go to work! 😒"
     else
-      @answer = "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work! 🤪"
     end
   end
 end
